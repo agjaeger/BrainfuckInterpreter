b45327063def94a41a520862db4bdbf7d305d21f
Alex-Jaeger <agjaeger@ualr.edu>
2014-11-25 12:34:58 -0600

    Added char by char read

--- lexer-parser/Lexer.java
+++ lexer-parser/Lexer.java
@@ -1,4 +1,9 @@
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 class Token {
 	public char cmd;
 	public int  numTimes;
@@ -11,7 +16,39 @@ class Token {
 
 
 public class Lexer {
-	public static void main(String[] args) {
+	private static int readFile(String filename) {
+		File file = new File(filename);
+   
+   		if (!file.exists()) {
+      		System.out.println(filename + " does not exist.");
+      		return 1;
+   		}
+    
+    	if (!(file.isFile() && file.canRead())) {
+      		System.out.println(file.getName() + " cannot be read from.");
+      		return 1;
+    	}
 
+    	try {
+    		FileInputStream fis = new FileInputStream(file);
+      		char current;
+      
+      		while (fis.available() > 0) {
+        		current = (char) fis.read();
+        		System.out.print(current);
+      		}
+    	} catch (IOException e) {
+      		e.printStackTrace();
+    		return 1;
+    	}
+
+    	return 0;
+	}
+
+
+	public static void main(String[] args) {
+		if (readFile(args[0]) == 1) {
+			System.out.println("Dammit there is an error");
+		}
 	}
 }
